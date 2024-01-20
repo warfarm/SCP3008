23a2321d9b04c82aae51bebd2f3dbfca1dadb3f4
warfarm <67772410+warfarm@users.noreply.github.com>
2024-01-20 17:13:10 -0500

    Base Commit

---
+++ SCP3008/Intermediate/Build/Win64/x64/SCP3008Editor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.38.33133
+#include "C:/Users/danz3/Downloads/SCP3008/SCP3008/Intermediate/Build/Win64/x64/SCP3008Editor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h"
---
+++ SCP3008/Source/SCP3008/SCP3008.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SCP3008.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SCP3008, "SCP3008" );
