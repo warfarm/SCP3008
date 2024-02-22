1c692eabc83beb92aa0550598bd48312be060641
AstralTurtle <jamesacs329@gmail.com>
2024-02-22 00:04:54 -0500

    we ball

---
+++ Source/SCP3008/Private/Entities/DefaultMob.cpp
@@ -0,0 +1,102 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "..\Public\Entities\DefaultMob.h"
+
+// define the static variables
+ TArray<TArray<float>> ADefaultMob::markovMatrix { {0.9f, 0.1f}, {0.1f, 0.9f} };
+
+// Sets default values
+ADefaultMob::ADefaultMob()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ActorState = PATROL;
+
+	
+	
+}
+void ADefaultMob::setMarkovChain(TArray<TArray<float>> newChain )
+{
+	markovMatrix = newChain;
+	
+}
+
+
+
+// Called when the game starts or when spawned
+void ADefaultMob::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
+// Called every frame
+void ADefaultMob::Tick(float DeltaTime)
+{
+	// testing code below
+	// SwitchState(ActorState, false);
+	// UE_LOG(LogTemp, Warning, TEXT("Current state: %d"), ActorState);
+	Super::Tick(DeltaTime);
+	
+}
+void ADefaultMob::Idle()
+{
+	
+	ActorState = IDLE;
+	FTimerHandle handle;
+	GetWorldTimerManager().SetTimer(handle, this, &ADefaultMob::SwitchState, 5.0f, false);
+}
+void ADefaultMob::Stun(float duration)
+{
+	ActorState = STUNNED;
+	FTimerHandle handle;
+	GetWorldTimerManager().SetTimer(handle, this, &ADefaultMob::Unstun, duration, false);
+}
+
+void ADefaultMob::Unstun()
+{
+	SwitchState(true);
+}
+
+// bOvveride default value set in header file
+void ADefaultMob::SwitchState(bool bOverride)
+{
+	float random = FMath::FRand();
+	if (ActorState == PATROL)
+	{
+		
+		if (random < markovMatrix[0][1])
+		{
+			ActorState = IDLE;
+		}
+	}
+	else if (ActorState == IDLE)
+	{
+		
+		if (random < markovMatrix[1][0])
+		{
+			ActorState = PATROL;
+		}
+	}
+	else if (bOverride)
+	{
+		
+		if (random < markovMatrix[0][1])
+		{
+			ActorState = IDLE;
+		}
+		else
+		{
+			ActorState = PATROL;
+		}
+	}
+	// Testing code below
+	
+	
+}
+
+
--- Source/SCP3008/Public/Entities/DefaultMob.h
+++ Source/SCP3008/Public/Entities/DefaultMob.h
@@ -4,23 +4,35 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "IkeaEmployee.generated.h"
+#include "DefaultMob.generated.h"
 
-enum EnemyState { PATROL, CHASE, IDLE, STUNNED};
+UENUM()
+enum EnemyState { PATROL UMETA(DisplayName = "Patrol"),
+	CHASE UMETA(DisplayName = "Chase"),
+	IDLE UMETA(DisplayName = "Idle"),
+	STUNNED UMETA(DisplayName = "STUNNED")};
 
 
 UCLASS()
-class SCP3008_API AIkeaEmployee : public AActor
+class SCP3008_API ADefaultMob : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	AIkeaEmployee();
+	ADefaultMob();
 	EnemyState ActorState;
+	float IdleTime{5.0f};
+	virtual void Stun(float duration);	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	// Exits stun state via SwitchState fucntion
+	virtual void Unstun();
+	// Idles the actor for a set duration
+	virtual void Idle();
+
+	
 
 	
 	
@@ -32,8 +44,10 @@ public:
 protected:
 	// controls switching between randomized states (patrol, idle)
 	// 0 = patrol, 1 = idle ie. [0][1] = chamce of going from patrol to idle
-	static TArray<TArray<float>> markovMatrix;
+	static  TArray<TArray<float>> markovMatrix;
 	// Switches state using the markov matrix. If bOverride is true, the state will be forced to change even if it isn't in a randomized state
 	// using a reference in current implementation, but could be changed to a return value 
-	void SwitchState(EnemyState& currentState, bool bOverride = false);
+	void SwitchState(bool bOverride = false);
+	virtual void setMarkovChain(TArray<TArray<float>>);
+
 };
