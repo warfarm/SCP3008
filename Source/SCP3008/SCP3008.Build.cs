cc536ac881620cdf3f003a42d75ceff02caff89c
AstralTurtle <jamesacs329@gmail.com>
2024-02-22 14:37:30 -0500

    me when i had to write csharp in unreal engine

--- Source/SCP3008/SCP3008.Build.cs
+++ Source/SCP3008/SCP3008.Build.cs
@@ -8,7 +8,7 @@ public class SCP3008 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "NavigationSystem", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
