8210371bcdfd2f4a795b6f23bfbff1493ba1c7dc
AstralTurtle <jamesacs329@gmail.com>
2024-03-12 11:52:05 -0400

    factory done

--- Source/SCP3008/Private/Faction.cpp
+++ Source/SCP3008/Private/Faction.cpp
@@ -6,11 +6,19 @@
 // Sets default values for this component's properties
 UFaction::UFaction()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
 
+	
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them
+	PrimaryComponentTick.bCanEverTick = false;
 	// ...
+}
+
+void UFaction::Initailize(int32 id, FColor color, std::string name)
+{
+	this->id = id;
+	this->color = color;
+	this->name = name;
 }
 
 
@@ -32,3 +40,45 @@ void UFaction::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	// ...
 }
 
+UFaction* UFaction::createDefaultFaction(AActor* Owner, int32 id)
+{
+	if (!Owner)
+	{
+		UE_LOG(LogTemp, Error, TEXT("UFaction::createDefaultFaction: Owner is null."));
+		return nullptr;
+	}
+
+	UFaction* NewFaction = nullptr;
+
+	switch (id)
+	{
+	case 0:
+		NewFaction = Owner->CreateDefaultSubobject<UFaction>(TEXT("Independent"));
+		NewFaction->Initailize(0, FColor::White, "Independent");
+		break;
+	case 1:
+		NewFaction = Owner->CreateDefaultSubobject<UFaction>(TEXT("SCPFoundation"));
+		NewFaction->Initailize(1, FColor::Blue, "SCP Foundation");
+		break;
+	case 2:
+		NewFaction = Owner->CreateDefaultSubobject<UFaction>(TEXT("ChaosInsurgency"));
+		NewFaction->Initailize(2, FColor::Red, "Chaos Insurgency");
+		break;
+	case 3:
+		NewFaction = Owner->CreateDefaultSubobject<UFaction>(TEXT("SerpentsHand"));
+		NewFaction->Initailize(3, FColor::Green, "Serpent's Hand");
+		break;
+	default:
+		UE_LOG(LogTemp, Error, TEXT("UFaction::createDefaultFaction: Invalid ID."));
+		return nullptr;
+	}
+	
+	return NewFaction;
+}
+
+
+int32 UFaction::generateId()
+{
+	static int32 id = 4;
+	return id++;
+}
--- Source/SCP3008/Public/Faction.h
+++ Source/SCP3008/Public/Faction.h
@@ -12,13 +12,36 @@ class SCP3008_API UFaction : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
+	int id;
+	FColor color;
+	std::string name;
+	
+	
+
+
+
 	// Sets default values for this component's properties
 	UFaction();
+	// Initialize the faction with an id, color, and name
+	void Initailize(int32, FColor, std::string);
+	
+	// create an instance of the default faction associated with that id
+	// 0 = Independent, 1 = SCP Foundation, 2 = Chaos Insurgency, 3 = Serpent's Hand
+	static UFaction* createDefaultFaction(AActor*,int32);	
+	// Generate unique id for custom factions
+	static int32 generateId();
+
+// Getters
+	int32 getId() const;
+	FColor getColor() const;
+	std::string_view getName() const;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	
 
 public:	
 	// Called every frame
