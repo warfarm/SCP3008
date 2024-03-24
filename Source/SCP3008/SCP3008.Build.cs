f49ba94f117e2a973dc8f6c0d9cd15fd898d3907
AstralTurtle <jamesacs329@gmail.com>
2024-03-24 11:59:39 -0400

    iris enabled

--- Source/SCP3008/SCP3008.Build.cs
+++ Source/SCP3008/SCP3008.Build.cs
@@ -11,6 +11,8 @@ public class SCP3008 : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "NavigationSystem", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "VivoxCore"});
+		
+		SetupIrisSupport(Target);
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
