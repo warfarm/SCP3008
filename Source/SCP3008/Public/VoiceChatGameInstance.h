d424a5a567996fbbf76b8051b98187f7599c957a
AstralTurtle <jamesacs329@gmail.com>
2024-03-24 11:26:11 -0400

    voice chat implemeted untested

---
+++ Source/SCP3008/Public/VoiceChatGameInstance.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "VivoxCore.h"
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "VoiceChatGameInstance.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class SCP3008_API UVoiceChatGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+	virtual void Init() override;
+	virtual void Shutdown() override;
+
+	void InitVivox();
+	void Login();
+	void JoinChannel();
+
+	IClient* VivoxVoiceClient;
+	AccountId LoggedInUserId;
+	
+};
