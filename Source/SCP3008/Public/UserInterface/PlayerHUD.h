bfc8b2a1fd0c80f8324852323598d5aee0aa1e18
warfarm <67772410+warfarm@users.noreply.github.com>
2024-05-30 21:13:10 -0400

    Start Player HUD Class

--- Source/SCP3008/Public/UserInterface/PlayerHUD.h
+++ Source/SCP3008/Public/UserInterface/PlayerHUD.h
@@ -9,6 +9,16 @@
 /**
  * 
  */
+
+UENUM(BlueprintType)
+enum class EHUDViewMode: uint8
+{
+	Clean UMETA(Tooltip="Clean, Basic HUD UI"),
+	Minimal UMETA(Tooltip="Minimal, Basic Necessities"),
+	Basic UMETA(Tooltip="Basic UI with necessities and additional features"),
+	Maximum UMETA(Tooltip="Extravagance Galore!")
+};
+
 UCLASS()
 class SCP3008_API APlayerHUD : public AHUD
 {
